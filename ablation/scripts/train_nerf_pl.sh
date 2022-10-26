795b2457cf8a0c2658e84b43fa945f49b368c26f
Alexander Freeman <alexander.freeman.nl@gmail.com>
2022-10-26 16:44:09 +0200

    Generalize train_nerf_pl script

--- ablation/scripts/train_nerf_pl.sh
+++ ablation/scripts/train_nerf_pl.sh
@@ -13,12 +13,14 @@ module load miniconda3
 
 conda activate nerf_pl
 
-python ../../papers/nerf_pl/train.py  \
+DATASET=kitti360-static
+
+python ../../papers/nerf_pl/train.py \
                 --dataset_name llff \ 
-                --root_dir ../datasets/kitti36-static/transforms \ 
+                --root_dir ../datasets/${DATASET}/transforms \ 
                 --N_importance 64 \ 
                 --img_wh 1408 376 \ 
                 --num_epochs 30 \ 
                 --batch_size 512 \
                 --optimizer adam --lr 5e-4 --lr_scheduler steplr --decay_step 10 20 --decay_gamma 0.5 \
-                --exp_name exp
+                --exp_name ${DATASET}
