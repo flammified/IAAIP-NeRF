99fb6c3919ed8e180755f6d9d5375a1444a6c64b
Alexander Freeman <alexander.freeman.nl@gmail.com>
2022-10-27 14:07:01 +0200

    Update nerf_pl train script

--- ablation/scripts/train_nerf_pl.sh
+++ ablation/scripts/train_nerf_pl.sh
@@ -13,6 +13,6 @@ module load miniconda3
 
 conda activate nerf_pl
 
-DATASET=kitti360-static
+DATASET=nuscenes-3
 
-python ../../papers/nerf_pl/train.py --dataset_name llff --root_dir ../../datasets/${DATASET}/llff --N_importance 64 --img_wh 1408 376 --num_epochs 30 --batch_size 512 --optimizer adam --lr 5e-4 --lr_scheduler steplr --decay_step 10 20 --decay_gamma 0.5 --exp_name ${DATASET}
+python ../../papers/nerf_pl/train.py --dataset_name llff --root_dir ../../datasets/${DATASET}/llff --N_importance 64 --img_wh 1600 900 --num_epochs 30 --batch_size 512 --optimizer adam --lr 5e-4 --lr_scheduler steplr --decay_step 10 20 --decay_gamma 0.5 --exp_name ${DATASET}
