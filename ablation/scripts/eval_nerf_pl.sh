3a93c284d12c1001f86dbf62e8c99c6e17e5362b
Alexander Freeman <alexander.freeman.nl@gmail.com>
2022-10-28 15:40:38 +0200

    Lower resource requirements of nerf_pl eval script

--- ablation/scripts/eval_nerf_pl.sh
+++ ablation/scripts/eval_nerf_pl.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #SBATCH --job-name="nerf-pl-example"
-#SBATCH --time=20:00:00
+#SBATCH --time=01:00:00
 #SBATCH --partition=gpu
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --gpus-per-task=2
+#SBATCH --gpus-per-task=1
 #SBATCH --mem-per-cpu=16384
 
 module load 2022r2
